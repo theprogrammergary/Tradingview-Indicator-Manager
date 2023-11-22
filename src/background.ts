2270c39692ea79f63c872b926b6c4ab347da8950
theprogrammergary <theprogrammergary@gmail.com>
2023-11-22 17:50:26 -0500

    feat: enable/disable proper html based on url

--- src/background.ts
+++ src/background.ts
@@ -1,4 +1,13 @@
-// background.ts
-chrome.runtime.onInstalled.addListener(function () {
-  console.log("Extension installed");
+const validUrl = "https://www.tradingview.com/script";
+
+chrome.action.onClicked.addListener(async (tab) => {
+    if (tab.id) {
+        if (tab.url?.startsWith(validUrl)) {
+            await chrome.action.setPopup({ tabId: tab.id, popup: 'dist/popup_valid.html' });
+            console.log("Extension enabled for the tab");
+        } else {
+            await chrome.action.setPopup({ tabId: tab.id, popup: 'dist/popup_invalid.html' });
+            console.log("Extension disabled for the tab");
+        }
+    }
 });
