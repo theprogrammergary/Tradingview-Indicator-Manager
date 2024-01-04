420846d80ab3a8b4301f1d52e96557091ccdb9cf
theprogrammergary <theprogrammergary@gmail.com>
2024-01-03 20:35:43 -0500

    create shared dir

--- desktop-app/src/shared/config.py
+++ desktop-app/src/shared/config.py
@@ -11,9 +11,13 @@ from loguru import logger
 # environment vars
 # load_dotenv(dotenv_path=os.path.join(os.getcwd(), ".setup", ".env"))
 
+
 # global vars
-RUNTIME_PATH: str = os.path.dirname(p=os.path.abspath(path=__file__))
-print(RUNTIME_PATH)
+RUNTIME_PATH: str = os.path.join(
+    os.path.dirname(p=os.path.abspath(path=__file__)), ".."
+)
+
+SESSION_FILE: str = os.path.join(RUNTIME_PATH, "shared", "session.json")
 
 
 # gui vars
@@ -21,10 +25,20 @@ GUI_WIDTH: int = 1000
 GUI_HEIGHT: int = 700
 GUI_NAME: str = "Tradingview Indicator Access Management"
 
+MESSAGE_WIDTH: int = 400
+MESSAGE_HEIGHT: int = 100
+
+
+# tradingview
+LOGIN_CHECK_URL: str = "https://www.tradingview.com/tvcoins/details/"
+ADD_ACCESS_URL: str = "https://www.tradingview.com/pine_perm/add/"
+REMOVE_ACCESS_URL: str = "https://www.tradingview.com/pine_perm/remove/"
+
+
 # logger
 logger.remove()
 logger.add(
-    sink="./logs/{time:YYYY-MM-DD}.log",
+    sink="./desktop-app/logs/{time:YYYY-MM-DD}.log",
     rotation="00:00",
     retention="14 days",
     backtrace=True,
